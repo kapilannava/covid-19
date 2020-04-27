28ab47c135c315341eb9dd092f5cedbbcc5abc33
Kapilan Navaratnam <kapilan@syomortgage.com>
2020-04-26 22:54:42 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+require('dotenv').config()
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () =>  {
+
+  const [deaths, setDeaths] = useState([]);
+ 
+  const getData = async () => {
+
+  const response= await fetch(`https://covid-19-statistics.p.rapidapi.com/reports/total/`, {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "covid-19-statistics.p.rapidapi.com",
+      "x-rapidapi-key": process.env.REACT_APP_API_KEY
+    }
+  })
+  const data = await response.json();
+  setDeaths(data.data);
+}
+
+    useEffect( ()=>{
+      getData();
+    }, []);
+
+return(
+  <div className="App">
+Last Updated: {deaths.last_update} <br />
+Confirmed cases: {deaths.confirmed}<br />
+Recovered: {deaths.recovered} <br />
+Deaths: {deaths.deaths} <br />
+Active cases: {deaths.active} <br />
+Fatality Rate: {deaths.fatality_rate}%
+</div>
+)
 }
 
 export default App;
