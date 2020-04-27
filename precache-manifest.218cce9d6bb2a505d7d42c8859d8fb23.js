bc181afd1442296971166938131fe8b28994ed3a
Kapilan Navaratnam <kapilan@syomortgage.com>
2020-04-26 22:55:29 -0400

    Updates

---
+++ precache-manifest.218cce9d6bb2a505d7d42c8859d8fb23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7e7c797c57dd9c7aa4696a4c8958079",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "07697911c1fec79af941",
+    "url": "/covid-19/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0c1442076ba26d3eb852",
+    "url": "/covid-19/static/js/2.3e89c456.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19/static/js/2.3e89c456.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07697911c1fec79af941",
+    "url": "/covid-19/static/js/main.25c9c025.chunk.js"
+  },
+  {
+    "revision": "268d38eb35bc30497802",
+    "url": "/covid-19/static/js/runtime-main.2531160a.js"
+  }
+]);
